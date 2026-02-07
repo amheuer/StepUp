9137ec6459e6f99b214eab003b78d73be8f0ec26
JackWilson05 <jacksonrwilson05@gmail.com>
2026-02-07 02:51:38 -0500

    integration of cv controls

---
+++ game.py
@@ -0,0 +1,12 @@
+"""
+StepUp: A minimal Doodle Jump clone using Pygame.
+- Player is a circle
+- Platforms are rectangles
+- No external assets required
+"""
+
+from game_core import Game
+
+
+if __name__ == '__main__':
+	Game().run()
